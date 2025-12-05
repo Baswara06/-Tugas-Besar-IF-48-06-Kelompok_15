801833be58cabfc6e90845597a8543c7322fd23c
azmi <azmihanif2005@gmail.com>
2025-12-05 09:37:55 +0700

    update

---
+++ Tugas-Besar-IF-48-06-Kelompok_15/penumpang.h
@@ -0,0 +1,44 @@
+#ifndef PENUMPANG_H_INCLUDED
+#define PENUMPANG_H_INCLUDED
+
+// ============================
+// PARENT (Temanmu yang buat)
+// ============================
+struct Ojek {
+    string idOjek;
+    string nama;
+    Ojek* next;
+    Ojek* prev;
+};
+
+// ============================
+// CHILD: PENUMPANG (DLL)
+// ============================
+struct Penumpang {
+    string idPenumpang;
+    string nama;
+    string noTelp;
+    string alamat;
+
+    Penumpang* next;
+    Penumpang* prev;
+};
+
+struct Relasi {
+    Penumpang* p;
+    Ojek* o;
+    Relasi* next;
+};
+
+Penumpang* createPenumpang(string id, string nama, string telp, string alamat);
+void insertPenumpang(Penumpang*& head, Penumpang* p);
+
+Relasi* createRelasi(Penumpang* p, Ojek* o);
+void insertRelasi(Relasi*& headRelasi, Relasi* r);
+
+Penumpang* findPenumpang(Penumpang* head, string id);
+
+void tampilPenumpangDenganOjek(Relasi* headRelasi);
+
+
+#endif // PENUMPANG_H_INCLUDED
