a048f6767bb6ec2a1c3705855acfe5b61c18b4ec
Minipss <azmihanif2005@gmail.com>
2025-12-07 17:51:00 +0700

    Ojolpenumpang.h

--- Tugas-Besar-IF-48-06-Kelompok_15/penumpang.h
+++ Tugas-Besar-IF-48-06-Kelompok_15/penumpang.h
@@ -6,6 +6,7 @@
 using namespace std;
 
 typedef struct PenumpangNode *adrPenumpang;
+typedef struct elemenOjol *adrOjol;
 
 struct PenumpangNode {
     string id;
@@ -19,6 +20,28 @@ struct ListPenumpang {
     adrPenumpang last;
 };
 
+struct ojol {
+    string id;
+    string nama;
+    adrOjol next;
+};
+
+struct elemenOjol {
+    ojol info;
+    adrOjol next;
+};
+
+// ojol
+void createListOjol(elemenOjol &L);
+adrOjol newOjol(string id, string nama);
+void insertFirstOjol(elemenOjol &L, adrOjol P);
+void insertLastOjol(elemenOjol &L, adrOjol P);
+void deleteFirstOjol(elemenOjol &L, adrOjol &P);
+void deleteLastOjol(elemenOjol &L, adrOjol &P);
+void deleteByIdOjol(elemenOjol &L, string id);
+adrPenumpang findOjol(elemenOjol L, string id);
+
+// penumpang
 void createListPenumpang(ListPenumpang &L);
 adrPenumpang newPenumpang(string id, string nama);
 void insertFirstPenumpang(ListPenumpang &L, adrPenumpang P);
@@ -27,7 +50,6 @@ void deleteFirstPenumpang(ListPenumpang &L, adrPenumpang &P);
 void deleteLastPenumpang(ListPenumpang &L, adrPenumpang &P);
 void deleteByIDPenumpang(ListPenumpang &L, string id);
 adrPenumpang findPenumpang(ListPenumpang L, string id);
-
 void showPenumpang(ListPenumpang L);
 
 
