a953b04539e68d2b9b747c70360bc661d4a68ed2
Minipss <azmihanif2005@gmail.com>
2025-12-19 17:33:56 +0700

    ojol.cpp

--- Tugas-Besar-IF-48-06-Kelompok_15/ojol.cpp
+++ Tugas-Besar-IF-48-06-Kelompok_15/ojol.cpp
@@ -1,107 +1,55 @@
-#include "ojol.h"
-#include <iostream>
-using namespace std;
+#include "Apk.h"
 
-void createListOjol(elemenOjol &L) {
-    L.first = nullptr;
-    L.last = nullptr;
-}
 
-adrOjol newOjol(string id, string nama) {
-    adrOjol P
-    p = new ojol;
-    P->id = id;
-    P->nama = nama;
-    P->prev = nullptr;
-    P->next = nullptr;
-    return P;
-}
+void createListOjol(ListOjol &L){ L.first = NULL; }
 
-void insertFirstOjol(elemenOjol &L, adrOjol P) {
-    if (L.first == nullptr) {
-        L.first = P;
-        L.last = P;
-    } else {
-        P->next = L.first;
-        L.first->prev = P;
-        L.first = P;
-    }
-}
 
-void inserLastOjol(elemenOjol &L, adrOjol P) {
-    if (L.first == nullptr) {
-        L.first = P;
-        L.last = P;
-    } else {
-        L.last->next = P;
-        P->prev = L.last;
-        L.last = P;
-    }
-}
-
-void deleteFirstOjol(elemenOjol &L, adrOjol &P) {
-    if (L.first == nullptr) {
-        P = nullptr;
-    } else if (L.first == L.last) {
-        P = L.first;
-        L.first = nullptr;
-        L.last = nullptr;
-    } else {
-        P = L.first;
-        L.first = L.first->next;
-        L.first->prev = nullptr;
-        P->next = nullptr;
-    }
-}
-
-void deleteLastOjol(elemenOjol &L, adrOjol &P) {
-    if (L.first == nullptr) {
-        P = nullptr;
-    } else if (L.first == L.last) {
-        P = L.first;
-        L.first = nullptr;
-        L.last = nullptr;
-    } else {
-        P = L.last;
-        L.last = L.last->prev;
-        L.last->next = nullptr;
-        P->prev = nullptr;
-    }
-}
-
-void deleteByIdOjol(elemenOjol &L, string id) {
-    adrOjol P = findPenumpang(L, id);
-    if (P == nullptr) return;
-
-    if (P == L.first) {
-        adrPenumpang temp;
-        deleteFirstPenumpang(L, temp);
-    } else if (P == L.last) {
-        adrPenumpang temp;
-        deleteLastPenumpang(L, temp);
-    } else {
-        P->prev->next = P->next;
-        P->next->prev = P->prev;
-        P->next = nullptr;
-        P->prev = nullptr;
-    }
-}
-
-adrPenumpang findOjol(elemenOjol L, string id) {
-    adrOjol P = L.first;
-    while (P != nullptr) {
-        if (P->id == id) return P;
-        P = P->next;
-    }
-    return nullptr;
-}
-
-void showOjol(elemenOjol L) {
-    adrOjol P = L.first;
-    while (P != nullptr) {
-        cout << "ID: " << P->id << " | Nama: " << P->nama << endl;
-        P = P->next;
-    }
+adrOjol createOjol(string id, string nama){
+adrOjol P = new Ojol;
+P->idOjol = id;
+P->nama = nama;
+P->next = NULL;
+P->relasiHead = NULL;
+return P;
 }
 
 
+void insertOjol(ListOjol &L, adrOjol P){
+if(L.first == NULL) L.first = P;
+else{
+adrOjol Q = L.first;
+while(Q->next != NULL) Q = Q->next;
+Q->next = P;
+}
+}
+
+
+adrOjol searchOjol(ListOjol L, string id){
+adrOjol P = L.first;
+while(P != NULL && P->idOjol != id) P = P->next;
+return P;
+}
+
+
+void deleteOjol(ListOjol &L, string id){
+adrOjol prec = NULL, P = L.first;
+while(P != NULL && P->idOjol != id){
+prec = P;
+P = P->next;
+}
+if(P != NULL){
+if(prec == NULL) L.first = P->next;
+else prec->next = P->next;
+delete P;
+}
+}
+
+
+void tampilOjol(ListOjol L){
+cout << "\nDaftar Ojol:\n";
+adrOjol P = L.first;
+while(P != NULL){
+cout << "- " << P->idOjol << " | " << P->nama << endl;
+P = P->next;
+}
+}
