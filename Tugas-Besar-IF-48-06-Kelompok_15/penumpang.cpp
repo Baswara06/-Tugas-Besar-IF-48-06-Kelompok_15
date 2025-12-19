9b8a5987213c7d23159338a4d443495e3037eba6
Minipss <azmihanif2005@gmail.com>
2025-12-19 17:48:32 +0700

    penumpang.cpp

--- Tugas-Besar-IF-48-06-Kelompok_15/penumpang.cpp
+++ Tugas-Besar-IF-48-06-Kelompok_15/penumpang.cpp
@@ -1,57 +1,73 @@
 #include "Apk.h"
 
 
-void createListPenumpang(ListPenumpang &L){ L.first = NULL; }
+void createListPenumpang(ListPenumpang &L){
+  L.first = NULL; 
+}
 
 
 adrPenumpang createPenumpang(string id, string nama){
-adrPenumpang C = new Penumpang;
-C->idPenumpang = id;
-C->nama = nama;
-C->next = NULL;
-C->prev = NULL;
-return C;
+  adrPenumpang C = new Penumpang;
+  C->idPenumpang = id;
+  C->nama = nama;
+  C->next = NULL;
+  C->prev = NULL;
+  return C;
 }
 
 
 void insertPenumpang(ListPenumpang &L, adrPenumpang C){
-if(L.first == NULL) L.first = C;
-else{
-adrPenumpang Q = L.first;
-while(Q->next != NULL) Q = Q->next;
-Q->next = C;
-C->prev = Q;
+  if(L.first == NULL){
+    L.first = C;
+  }else{
+  adrPenumpang Q = L.first;
+  while(Q->next != NULL){
+    Q = Q->next;
+    Q->next = C;
+    C->prev = Q;
+  }
 }
 }
 
 
 adrPenumpang searchPenumpang(ListPenumpang L, string id){
-adrPenumpang C = L.first;
-while(C != NULL && C->idPenumpang != id) C = C->next;
-return C;
+  adrPenumpang C ;
+  C = L.first;
+  while(C != NULL && C->idPenumpang != id){
+    C = C->next;
+  }
+  return C;
 }
 
 
 void deletePenumpang(ListPenumpang &L, string id){
-adrPenumpang C = searchPenumpang(L, id);
-if(C != NULL){
-if(C->prev != NULL) C->prev->next = C->next;
-else L.first = C->next;
-
-
-if(C->next != NULL) C->next->prev = C->prev;
-
-
-delete C;
+    adrPenumpang C = searchPenumpang(L, id);
+    if (C == NULL) {
+        return;
+    }
+    if (C == L.first) {
+        L.first = C->next;
+        if (L.first != NULL) {
+            L.first->prev = NULL;
+        }
+    }else {
+        C->prev->next = C->next;
+        if (C->next != NULL) {
+            C->next->prev = C->prev;
+        }
+    }
+    C->next = NULL;
+    C->prev = NULL;
 }
-}
+
 
 
 void tampilPenumpang(ListPenumpang L){
-cout << "\nDaftar Penumpang:\n";
-adrPenumpang C = L.first;
-while(C != NULL){
-cout << "- " << C->idPenumpang << " | " << C->nama << endl;
-C = C->next;
-}
+  cout << "\nDaftar Penumpang:\n";
+  adrPenumpang C ;
+  C = L.first;
+  while(C != NULL){
+    cout << "- " << C->idPenumpang << " | " << C->nama << endl;
+    C = C->next;
+  }
 }
