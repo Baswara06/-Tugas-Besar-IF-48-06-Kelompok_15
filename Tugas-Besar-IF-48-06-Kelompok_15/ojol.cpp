c225d922f7bca438a77282e274b293c7e802017b
Minipss <azmihanif2005@gmail.com>
2025-12-19 17:40:10 +0700

    ojol.cpp

--- Tugas-Besar-IF-48-06-Kelompok_15/ojol.cpp
+++ Tugas-Besar-IF-48-06-Kelompok_15/ojol.cpp
@@ -1,55 +1,62 @@
 #include "Apk.h"
 
 
-void createListOjol(ListOjol &L){ L.first = NULL; }
+void createListOjol(ListOjol &L){
+  L.first = NULL;
+}
 
-
-adrOjol createOjol(string id, string nama){
-adrOjol P = new Ojol;
-P->idOjol = id;
-P->nama = nama;
-P->next = NULL;
-P->relasiHead = NULL;
-return P;
+adrOjol createOjol(string id, string nama) {
+  adrOjol P = new Ojol;
+  P->idOjol = id;
+  P->nama = nama;
+  P->next = NULL;
+  P->relasiHead = NULL;
+  return P;
 }
 
 
 void insertOjol(ListOjol &L, adrOjol P){
-if(L.first == NULL) L.first = P;
-else{
-adrOjol Q = L.first;
-while(Q->next != NULL) Q = Q->next;
-Q->next = P;
+  if(L.first == NULL){ 
+    L.first = P;
+  }else{
+    adrOjol Q = L.first;
+  while(Q->next != NULL) Q = Q->next;
+  Q->next = P;
 }
 }
 
 
 adrOjol searchOjol(ListOjol L, string id){
 adrOjol P = L.first;
-while(P != NULL && P->idOjol != id) P = P->next;
+while(P != NULL && P->idOjol != id){ 
+  P = P->next;
+}
 return P;
 }
 
 
 void deleteOjol(ListOjol &L, string id){
-adrOjol prec = NULL, P = L.first;
-while(P != NULL && P->idOjol != id){
-prec = P;
-P = P->next;
+  adrOjol prec = NULL, P = L.first;
+  while(P != NULL && P->idOjol != id){
+    prec = P;
+    P = P->next;
+  }
+  if(P != NULL){
+    if(prec == NULL){
+      L.first = P->next;
+    }else {
+      prec->next = P->next;
+      delete P;
 }
-if(P != NULL){
-if(prec == NULL) L.first = P->next;
-else prec->next = P->next;
-delete P;
 }
 }
 
 
 void tampilOjol(ListOjol L){
-cout << "\nDaftar Ojol:\n";
-adrOjol P = L.first;
-while(P != NULL){
-cout << "- " << P->idOjol << " | " << P->nama << endl;
-P = P->next;
+  cout << "\nDaftar Ojol:\n";
+  adrOjol P = L.first;
+  while(P != NULL){
+  cout << "- " << P->idOjol << " | " << P->nama << endl;
+  P = P->next;
 }
 }
